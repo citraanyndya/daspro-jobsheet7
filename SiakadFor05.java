84c2f6c45a69986c4b29c5b2a3fef84cdf74a57d
citrarpl2 <toraltte.wavs@gmail.com>
2025-10-22 10:46:54 +0700

    modifikasi

--- SiakadFor05.java
+++ SiakadFor05.java
@@ -5,23 +5,32 @@ public class SiakadFor05 {
         
         Scanner sc = new Scanner(System.in);
 
-        double nilaiTertinggi;
-        double nilaiTerendah;
-        double nilai, tertinggi = 0, terendah = 100;
-        
+        double nilai;
+        double tertinggi = 0, terendah = 100;
+        int jumlahLulus = 0, jumlahTidakLulus = 0; 
+
         for (int i = 1; i <= 10; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-" + i + ": ");
             nilai = sc.nextDouble();
+
             if (nilai > tertinggi) {
                 tertinggi = nilai;
             }
             if (nilai < terendah) {
                 terendah = nilai;
             }
+
+            if (nilai >= 60) {
+                jumlahLulus++; 
+            } else {
+                jumlahTidakLulus++; 
+            }
         }
 
-        System.out.println("Nilai tertinggi: " + tertinggi);
-        System.out.println("Nilai terendah: " + terendah);
-        
+        System.out.println("\n=== HASIL AKHIR ===");
+        System.out.println("Nilai tertinggi : " + tertinggi);
+        System.out.println("Nilai terendah  : " + terendah);
+        System.out.println("Jumlah mahasiswa lulus : " + jumlahLulus);
+        System.out.println("Jumlah mahasiswa tidak lulus : " + jumlahTidakLulus);
     }
 }
